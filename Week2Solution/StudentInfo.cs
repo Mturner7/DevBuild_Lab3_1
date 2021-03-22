a8f9a598b9977b0a20b14d2776c50cdf65908cc4
Mturner7 <80474793+Mturner7@users.noreply.github.com>
2021-03-22 16:56:12 -0400

    Updated

--- Week2Solution/StudentInfo.cs
+++ Week2Solution/StudentInfo.cs
@@ -27,23 +27,81 @@ namespace StudentInfo
             "Mochi", "Cold butter", 
             "Shrimp", "Ice cubes", 
             "Raw eggs", "The blood of his enemies", 
-            "Watermelon", "Anything ball shaped", 
+            "Watermelon", "Sodium", 
             "Lukewarm soup", "Ice cream"
         };
 
         private static void viewAllStudents()
         {
             int i = 1;
+
+            Console.WriteLine();
             foreach (string student in students)
             {
                 Console.WriteLine($"{i}. {student}");
                 i++;
             }
+            Console.WriteLine();
         }
 
         public static void Main(string[] args)
         {
-            
+            int index;
+            string student;
+            string title;
+            string food;
+            string input;
+            bool done = false;
+
+            while (!done)
+            {
+                Console.Write("Which student do you want to learn about? (enter a number 1-9 or '10' to view all students): ");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out index);
+                
+                while (index < 1 || index > 10)
+                {
+                    Console.Write("Please enter a valid integer between 1 and 9 or '10' to view all the students: ");
+                    input = Console.ReadLine();
+                    Int32.TryParse(input, out index);
+                }
+
+                if (index == 10)
+                {
+                    viewAllStudents();
+                    continue;
+                }
+
+                student = students[index - 1];
+                title = previousTitles[index - 1];
+                food = favoriteFoods[index - 1];
+
+                Console.WriteLine($"\nStudent {index} is {student}.\n");
+
+                Console.Write("What would you like to learn about this student? (favorite food / previous title ): ");
+                input = Console.ReadLine().ToLower();
+
+                while (!input.Contains("food") && !input.Contains("title"))
+                {
+                    Console.Write("That category is non-existent, please enter a  valid category! (favorite food/previous title): ");
+                    input = Console.ReadLine();
+                }
+
+                if (input.Contains("food")) Console.WriteLine($"\n{student}s favorite food is '{food}'\n");
+                else if (input.Contains("title")) Console.WriteLine($"\n{student}s previous title was '{title}'\n");
+
+                do
+                {
+                    Console.Write("Would you like to continue? (enter 'y' or 'n'): ");
+                    input = Console.ReadLine();
+
+                } while (input != "y" && input != "n");
+
+                if (input == "n") done = true;
+
+            } 
+
+            Console.WriteLine("Thank you!");
         }
     }
 }
